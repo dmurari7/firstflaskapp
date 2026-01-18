72238c7cd1b3f50c0e40b384677ec2e163ac7a74
dmurari7 <dharshaan@hotmail.com>
2026-01-18 19:42:12 -0400

    status code specification

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "<h1>Hello, World!</h1>"
 
+#adding second parameter after return to specify status code
+@app.route('/hello')
+def hello():
+    response = make_response("Hello, World!\n")
+    response.status_code = 202
+    response.headers['content-type'] = 'text/plain'
+    return response
+
+
 #methods param indicates which HTTP methods are allowed
 @app.route('/helloWorld', methods=['GET', 'POST'])
-def hello():
+def hello_world():
     #create form to get information from user
     if request.method == 'GET':
         return 'You made a GET request' 
