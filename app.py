dae9be9366a8a8335268e0eb4a3dbee87bb5ee95
dmurari7 <dharshaan@hotmail.com>
2026-01-18 19:08:23 -0400

    dynamic route examples

--- app.py
+++ app.py
@@ -5,5 +5,19 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello, World!</h1>"
 
+@app.route('/helloWorld')
+def hello():
+    return "Hello world!"
+
+#dynamic route examples
+@app.route('/greet/<name>')
+def great(name):
+    return f"Hello, {name}!"
+
+@app.route('/add/<int:number1>/<int:number2>')
+def add(number1, number2):
+    return f'{number1} + {number2} = {number1 + number2}'
+    
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
