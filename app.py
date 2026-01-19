c5e98c9534c658f7ab94999a1cd0d0df3da21d52
dmurari7 <dharshaan@hotmail.com>
2026-01-19 16:42:24 -0400

    added html content; implemented filters and URLS

--- app.py
+++ app.py
@@ -1,48 +1,25 @@
-from flask import Flask, request, make_response
-app = Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
-    return "<h1>Hello, World!</h1>"
+    my_list = [1, 2, 3, 4, 5]
+    return render_template('index.html', my_list=my_list)
 
-#adding second parameter after return to specify status code
-@app.route('/hello')
-def hello():
-    response = make_response("Hello, World!\n")
-    response.status_code = 202
-    response.headers['content-type'] = 'text/plain'
-    return response
+@app.route('/other')
+def other():
+    some_text = "This is some other text."
+    return render_template('other.html', some_text=some_text)
 
+#filters
+@app.template_filter('reverse')
+def reverse_filter(str):
+    return str[::-1]
 
-#methods param indicates which HTTP methods are allowed
-@app.route('/helloWorld', methods=['GET', 'POST'])
-def hello_world():
-    #create form to get information from user
-    if request.method == 'GET':
-        return 'You made a GET request' 
-    #process the form data
-    elif request.method == 'POST':
-        return 'You made a POST request' 
-    else: 
-        return 'Unsupported HTTP method'
-
-#dynamic route examples
-@app.route('/greet/<name>')
-def great(name):
-    return f"Hello, {name}!"
-
-@app.route('/add/<int:number1>/<int:number2>')
-def add(number1, number2):
-    return f'{number1} + {number2} = {number1 + number2}'
-
-@app.route('/handle_url_params')
-def handle_params():
-    if 'greeting' in request.args.keys() and 'name' in request.args.keys():
-        greeting = request.args['greeting']
-        name = request.args['name']
-        return f'{greeting}, {name}!'
-    else:
-        return 'Error: not enough parameters'
+@app.template_filter('repeat')
+def repeat_filter(str, times=2):
+    return str * times
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
