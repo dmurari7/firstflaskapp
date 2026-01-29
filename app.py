13fce75522e81c09ad375a85840f90c00e106dc1
dmurari7 <dharshaan@hotmail.com>
2026-01-28 21:39:46 -0400

    more stuff to commit for venv

--- app.py
+++ app.py
@@ -1,12 +1,49 @@
-from flask import Flask, render_template
+from turtle import pd
+from flask import Flask, render_template, request, Response
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == 'dharshaan' and password == 'hi':
+            return 'Successfully logged in'
+        else:
+            return 'Invalid credentials'
+        
+@app.route('/file-upload', methods=['POST'])
+def file_upload():
+    file = request.files['file']
+
+    if file.content_type == 'text/plain':
+        return file.read().decode('utf-8')
+    elif file.content_type == 'application/pdf':
+        content = 'PDF file uploaded successfully.'
 
 
-    
+@app.route('/convert_csv', methods=['POST'])
+def convert_csv():
+    file = request.files['file']
+
+    df = pd.read_excel(file)
+
+    response = Response(
+        df.to_csv(),
+        mimetype='text/csv',
+        headers={
+            'Content-Disposition': 'attachment; filename=result.csv'
+        }
+    )
+
+    return response
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
